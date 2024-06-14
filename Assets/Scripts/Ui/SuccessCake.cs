b8f0dfc1095c8f08cb787a8b70955fc741446bc7
René Jensen <rene.jensen@sybogames.com>
2024-06-14 12:06:50 +0200

    Updated loss and win screen

---
+++ Assets/Scripts/Ui/SuccessCake.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace BakingGame
+{
+	public class SuccessCake : MonoBehaviour
+	{
+		public Sprite[] Cakes;
+		public Image Image;
+
+		void OnEnable()
+		{
+			Image.sprite = Cakes[(int)(Cakes.Length * Random.value)];
+		}
+	}
+}
